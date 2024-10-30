197478cc04b7a54d6698848e0831b49a64c6ed60
luz squarzon <squarzonrp@gmail.com>
2024-10-30 12:29:44 -0300

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    //hola mundo 
     specPattern: 'cypress/e2e/**/*.js'
   },
 });
